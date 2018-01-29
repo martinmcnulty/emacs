8eaa4f3dc1808d0226068cfadc63f07cc334d53f
Martin McNulty <martin.mcnulty@ovoenergy.com>
2018-01-29 10:06:58 +0000

    Set Javascript indent level to two spaces

--- init.el
+++ init.el
@@ -74,6 +74,7 @@
      ("#A75B00" . 70)
      ("#F309DF" . 85)
      ("#3C3D37" . 100))))
+ '(js-indent-level 2)
  '(magit-diff-section-arguments (quote ("--ignore-space-change" "--no-ext-diff")))
  '(magit-diff-use-overlays nil)
  '(package-selected-packages
